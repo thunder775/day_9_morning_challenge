084f932b20756ee38ebac53d6991518646393f4f
McLarenCollege <hello@mclarencollege.in>
2019-10-11 08:21:51 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,52 @@
+main() {
+
+}
+
+// Challenge 1 - Purge and Organize
+// Given a list of numbers, write a function that returns an list that...
+// 1. Has all duplicate elements removed .
+// 2. Is sorted from least to greatest value.
+// Examples
+// uniqueSort([1, 2, 4, 3]) ➞ [1, 2, 3, 4]
+// uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) ➞ [1, 2, 3, 4]
+// uniqueSort([6, 7, 3, 2, 1]) ➞ [1, 2, 3, 6, 7]
+
+
+// Challenge 2 - Sum of all Even Numbers in a Matrix
+// Create a function that returns the sum of all even elements in a 2D matrix.
+// Examples
+// sumOfEvens([
+//   [1, 0, 2],
+//   [5, 5, 7],
+//   [9, 4, 3]
+// ]) ➞ 6
+//
+// 2 + 4 = 6
+//
+// sumOfEvens([
+//   [1, 1],
+//   [1, 1]
+// ]) ➞ 0
+//
+// sumOfEvens([
+//   [42, 9],
+//   [16, 8]
+// ]) ➞ 66
+//
+// sumOfEvens([
+//   [],
+//   [],
+//   []
+// ]) ➞ 0
+
+
+// Challenge 3 - Unmix My Strings
+// 'lPaeesh le pemu mnxit ehess rtnisg!' Oh, sorry, that was supposed to say: Please help me unmix these strings!
+// Somehow my strings have all become mixed up; every pair of characters has been swapped.
+// Help me undo this so I can understand my strings again.
+// Example:
+// unmix("123456") ➞ "214365"
+//
+// unmix("hTsii  s aimex dpus rtni.g") ➞ "This is a mixed up string."
+//
+// unmix("badce") ➞ "abcde"
